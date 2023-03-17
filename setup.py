ff4753de843ce2f4a7b727d16e94151cea81ff3f
Tylar <email@tylar.info>
2023-03-17 15:33:51 -0400

    fix version file path

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup
 
+PACKAGE_NAME = "erddap_datasetsxml_builder"
+
 setup(
     # The package metadata is specified in setup.cfg but GitHub's downstream dependency graph
     # does not work unless we put the name this here too.
-    name="erddap_datasetsxml_builder",
+    name=f"{PACKAGE_NAME}",
     use_scm_version={
-        "write_to": "pydwcviz/_version.py",
+        "write_to": f"{PACKAGE_NAME}/_version.py",
         "write_to_template": '__version__ = "{version}"',
         "tag_regex": r"^(?P<prefix>v)?(?P<version>[^\+]+)(?P<suffix>.*)?$",
     },
