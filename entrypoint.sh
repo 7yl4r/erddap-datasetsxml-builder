f511856a2ef7776de30f6b1e2298f7aed7136924
Tylar <email@tylar.info>
2023-03-17 14:47:53 -0400

    fix(?) path to builder script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-python build_datasetsxml.py ${INPUT_DATASETS_DIR}
+python erddap_datasetsxml_builder/build_datasetsxml.py ${INPUT_DATASETS_DIR}
 
 # echo "::set-output name=successful_dataset_count::$TODO_GET_THAT_OUTPUT_HERE_SOMEHOW"
