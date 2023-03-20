7853d3ed87b97eef3e34b7dd5e9bc842451ea54b
Tylar <email@tylar.info>
2023-03-20 16:16:38 -0400

    rm tree

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 #set -e
 
-ls -lah .
-
-tree
-
 python erddap_datasetsxml_builder/build_datasetsxml.py ${INPUT_DATASETS_DIR}
 
 # echo "::set-output name=successful_dataset_count::$TODO_GET_THAT_OUTPUT_HERE_SOMEHOW"
